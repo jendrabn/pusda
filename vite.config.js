60fb43f46eab9f1d38145d4678aabdeca7d343dc
jendrabn <jendraabw@gmail.com>
2025-10-05 14:04:09 +0700

    Refactor Vite configuration to remove adminlte.scss input from plugins

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
-  plugins: [
-    laravel({
-      input: ["resources/scss/front/style.scss", 'resources/scss/adminlte4/adminlte.scss'],
-      refresh: true,
-    }),
-  ],
+	plugins: [
+		laravel({
+			input: ["resources/scss/front/style.scss"],
+			refresh: true,
+		}),
+	],
 });
